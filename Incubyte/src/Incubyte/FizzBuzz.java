1bd1a092ed0e7a906f346a6926a4dc0c9082145f
Shashikala <Shashikala@DESKTOP-0J95I56>
2020-10-26 11:19:45 +0530

    Incubyte

---
+++ Incubyte/src/Incubyte/FizzBuzz.java
@@ -0,0 +1,23 @@
+package Incubyte;
+public class FizzBuzz {
+	static public void main(String args[])
+	{
+		for(int i=1;i<=100;i++)
+		{
+			if(i%3==0 && i%5==0) {
+				System.out.println("FizzBuzz");
+			}
+			else if(i%3==0)
+			{
+				System.out.println("Fizz");
+			}
+			else if(i%5==0)
+			{
+				System.out.println("Buzz");
+			}
+			System.out.println(i);
+		}
+		
+	}
+
+}
